3f863e03d64b6f76015ca14c5cfa0a13b4cd1369
EduardoRu <482000322@alumnos.utzac.edu.mx>
2022-07-07 12:48:30 -0500

    Tercera afiniación del sistema

--- app/Http/Controllers/genExcel.php
+++ app/Http/Controllers/genExcel.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Exports\Consultas;
+use App\Models\Clave_alumno;
+use App\Models\Grupo;
+use App\Models\Encuest;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -15,7 +18,7 @@ class genExcel extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     /**
      * Display the specified resource.
      *
@@ -28,9 +31,25 @@ class genExcel extends Controller
         $idGrupo = intval($request->input('IDG'));
         $idEscuela = intval($request->input('IDE'));
 
-        //return [$idAlumno, $idGrupo, $idEscuela];
+        $nomExcel = "";
 
-        return (new Consultas($idAlumno, $idGrupo, $idEscuela))->download('invoices.xlsx');
+        if ($idAlumno != 0 && $idGrupo != 0 && $idEscuela != 0) {
+            $Alumno = Clave_alumno::find($idAlumno);
+            $Grupo = Grupo::find($idGrupo);
+            $Escuela = Encuest::find($idEscuela);
+
+            $nomExcel = $Alumno->nombre_alumno."-".$Grupo->grado.$Grupo->grupo."-".$Escuela->nombre_institucion;
+
+        }else if($idGrupo != 0 && $idEscuela != 0){
+            $Grupo = Grupo::find($idGrupo);
+            $Escuela = Encuest::find($idEscuela);
+
+            $nomExcel = $Grupo->grado.$Grupo->grupo."-".$Escuela->nombre_institucion;
+        }else if($idEscuela != 0){
+            $Escuela = Encuest::find($idEscuela);
+            $nomExcel = $Escuela->nombre_institucion;
+        }
+        return (new Consultas($idAlumno, $idGrupo, $idEscuela))->download($nomExcel.'.xlsx');
 
     }
 }
