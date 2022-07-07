a195f2eed59e7587742a93a5d842d6d83e1cef06
EduardoRu <482000322@alumnos.utzac.edu.mx>
2022-07-06 20:45:47 -0500

    Ultima edicón

--- app/Exports/Consultas.php
+++ app/Exports/Consultas.php
@@ -57,10 +57,24 @@ class Consultas implements FromQuery, WithHeadings, WithColumnWidths, ShouldAuto
     public function columnWidths(): array
     {
         return [
-            'C' => 6,
-            'D' => 6,
+            'C' => 7,
+            'D' => 7,
             'F' => 10,
-            'G' => 10         
+            'G' => 10,
+            'H' => 10,
+            'I' => 31,
+            'J' => 15,
+            'K' => 15,
+            'L' => 20,
+            'M' => 15,
+            'N' => 10,
+            'O' => 10,
+            'P' => 25,
+            'Q' => 20,
+            'R' => 20,
+            'S' => 20,
+            'T' => 20,
+            'U' => 20
         ];
     }
 
@@ -105,7 +119,35 @@ class Consultas implements FromQuery, WithHeadings, WithColumnWidths, ShouldAuto
             ->where('clave_alumnos.idclave_alumno', $this->idAlumno);
 
         }else if($this->idGrupo != 0 and $this->idEscuela != 0){
-            return Grupo::query()->where('idgrupos', $this->idGrupo);
+            return Grupo::query()
+            ->select(
+                DB::raw('nombre_alumno'),
+                DB::raw('sexo'),
+                DB::raw('grupos.grado'),
+                DB::raw('grupos.grupo'),
+                DB::raw('encuestas.nombre_institucion'),
+                DB::raw('resultados.salud_mental'),
+                DB::raw('resultados.sistema_familiar'),
+                DB::raw('resultados.presion_padres'),
+                DB::raw('resultados.disp_sustancias_expect_consumo'),
+                DB::raw('resultados.persepcion_riesgo->"$.Tabaco"'),
+                DB::raw('resultados.persepcion_riesgo->"$.Alcohol"'),
+                DB::raw('resultados.persepcion_riesgo->"$.Otras_drogas"'),
+                DB::raw('resultados.persepcion_riesgo->"$.total"'),
+                DB::raw('resultados.desempeno_escolar'),
+                DB::raw('resultados.violencia'),
+                DB::raw('resultados.riesgo_inicio_incremento_consumo'),
+                DB::raw('resultados.consumo_sustancias->"$.Tabaco"'),
+                DB::raw('resultados.consumo_sustancias->"$.Alcohol"'),
+                DB::raw('resultados.consumo_sustancias->"$.Otras_drogas"'),
+                DB::raw('resultados.consumo_sustancias->"$.total"'),
+                DB::raw('resultados.participacion_acciones_preventivas'),
+                DB::raw('resultados.IVG'))
+            ->join('clave_alumnos', 'grupos.idgrupos', '=', 'clave_alumnos.idgrupo')
+            ->join('encuestas', 'grupos.idencuesta', '=', 'encuestas.idencuesta')
+            ->join('resultados', 'clave_alumnos.idclave_alumno', '=', 'resultados.idclave_alumno')
+            ->where('grupos.idgrupos', $this->idGrupo);
+
         }else if($this->idEscuela != 0){
             return Encuest::query()->where('idencuesta', $this->idEscuela);
         }
